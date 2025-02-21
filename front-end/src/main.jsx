d4be6a5f20b3acedef3530c10a6011f80f821988
Sina <ghadimi.cna@gmail.com>
2025-02-21 00:15:13 -0800

    deleting the event list as it will need to be its own react component and has to be callable more than 1 time

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App/App.jsx'
-
+import Events from './Tests/test_Events.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Events/>
   </StrictMode>,
 )
