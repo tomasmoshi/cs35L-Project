128429400bae0d5dc296bb1581227b0c30bec5b4
Sina <ghadimi.cna@gmail.com>
2025-02-21 17:31:34 -0800

    Fixed the event structure now We can use each of this event components seperately, now each component has its own dedicated css as well, and they work with backend properly

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App/App.jsx'
-import Events from './Tests/test_Events.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <Events/>
+    <App />
   </StrictMode>,
 )
