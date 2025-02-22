34d8a010a170cb1f63af52cd7dcf30481f592b40
Shervinsasl <136936840+Shervinsasl@users.noreply.github.com>
2025-02-22 13:16:27 -0800

    Merging main to my branch

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,19 +1,33 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useEffect, useState } from "react";
+import React from "react";
+import Events from "./Events"; // Import the Events component
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/")
-      .then((response) => response.json())
-      .then((data) => setData(data.message))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-
-  return <div>{data ? data : "Loading..."}</div>;
+  return (
+    <div className="App">
+      <h1>My Events App</h1>
+      <Events />
+    </div>
+  );
 }
 
 export default App;
+
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import { useEffect, useState } from "react";
+// import Events from './Events';
+
+// function App() {
+//   const [data, setData] = useState(null);
+//   useEffect(() => {
+//     fetch("http://127.0.0.1:8000/")
+//       .then((response) => response.json())
+//       .then((data) => setData(data.message))
+//       .catch((error) => console.error("Error fetching data:", error));
+//   }, []);
+
+//   return <div>{data ? data : "Loading..."}</div>;
+// }
+
+// export default App;
