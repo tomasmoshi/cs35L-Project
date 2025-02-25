4cafc275101ea9518c74ab565365c6f969621766
Sina <ghadimi.cna@gmail.com>
2025-02-25 02:23:19 -0800

    users model is working in backend, tested with the signup page

--- AllExercises/users/models.py
+++ AllExercises/users/models.py
@@ -9,9 +9,6 @@ class UserProfile(models.Model):
     profile_image = models.ImageField(
         default="default.jpg", upload_to="profile_images/", blank=True, null=True
     )
-    name = models.CharField(max_length=300)
-    last_name = models.CharField(max_length=300)
-    email = models.EmailField(unique=True)
 
     def __str__(self):
         return f"{self.user.username} Profile"
--- AllExercises/users/serializers.py
+++ AllExercises/users/serializers.py
@@ -1,10 +1,26 @@
 from rest_framework import serializers
-from taggit.serializers import TagListSerializerField, TaggitSerializer
+from django.contrib.auth.models import User
+
 from .models import UserProfile
 
 
 class UserSerializer(serializers.ModelSerializer):
-
+    # Allow these fields to be provided by the client.
+    username = serializers.CharField(source='user.username')
+    password = serializers.CharField(source='user.password', write_only=True)
+    email = serializers.EmailField(source='user.email')
+    first_name = serializers.CharField(source = 'user.first_name')
+    last_name = serializers.CharField(source = 'user.last_name')
+    
     class Meta:
         model = UserProfile
-        fields = ["__all__"]
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password']
+    
+    def create(self, validated_data):
+        # Extract nested user data.
+        user_data = validated_data.pop('user')
+        # Create the User with the provided data.
+        user = User.objects.create_user(**user_data)
+        # Create the profile linked to the user.
+        profile = UserProfile.objects.create(user=user, **validated_data)
+        return profile
