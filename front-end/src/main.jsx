092bb61970db31a5cf62eb6a68fec95e937d8df0
tomasmoshi <tomas.alosh@gmail.com>
2025-03-02 19:56:25 -0800

    pulled from main

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,9 +3,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App/App.jsx'
-
-
-import App from './App/App.jsx'
 import EventForm from './Events/EventForm.jsx'
 import EventList from './Events/EventList.jsx'
 const handleEventSubmitted = (newEvent) => {
