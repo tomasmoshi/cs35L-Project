fe7eaf0a0e8512cf05a7edfc0b0b8c91a7b87ece
tomasmoshi <tomas.alosh@gmail.com>
2025-03-02 20:06:58 -0800

    fixing user page. Added a login feature with a profile icon in the front page, Connected the backend to the front end

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -13,7 +13,6 @@ const handleEventSubmitted = (newEvent) => {
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <App />
     {/* <EventForm onEventSubmitted={handleEventSubmitted}/>
     <EventList /> */}
   </StrictMode>
