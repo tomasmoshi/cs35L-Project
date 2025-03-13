29c09c4616c373cde7dce10ec72a47bff5739824
Sina <ghadimi.cna@gmail.com>
2025-03-12 19:44:49 -0700

    fixed styling on help, about, login, searchbar and post events, signup left

--- front-end/src/Features/PostEvent/PostEvent.jsx
+++ front-end/src/Features/PostEvent/PostEvent.jsx
@@ -3,8 +3,9 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import EventForm from "../../Events/EventForm"; // Adjust the path as needed
 import Login from "../Login/Login"; // Adjust the path as needed
-import "../Help/HelpModal.css";
-
+import "../Help/HelpModal.css"
+import "../../Events/EventForm.css"
+import "./PostEvent.css"
 const PostEvent = ({ onEventSubmitted, onClose }) => {
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
@@ -18,12 +19,14 @@ const PostEvent = ({ onEventSubmitted, onClose }) => {
   };
 
   return (
-    <div className="modal">
-      {token ? (
-        <EventForm onEventSubmitted={onEventSubmitted} onClose={handleClose} />
-      ) : (
-        <Login onClose={handleClose} />
-      )}
+    <div className="post-event-modal">
+      <div className="event-form-wrapper">
+        {token ? (
+          <EventForm onEventSubmitted={onEventSubmitted} onClose={handleClose} />
+        ) : (
+          <Login onClose={handleClose} />
+        )}
+      </div>
     </div>
   );
 };
